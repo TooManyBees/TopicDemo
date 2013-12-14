cc71bf5ab4b9014a9d4786425751710dac4a25ae
Jesse Kipp <esbe.v2@gmail.com>
2013-12-13 19:31:17 -0500

    Adds quote snippet to comment
    If a comment is not yet persisted and it has a valid parent_id
    (another comment), it will grab the parent's content and assign it to
    the new comment's quote attribute.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,25 +7,13 @@ class CommentsController < ApplicationController
   def create
     discussion = Discussion.find(params[:discussion_id])
     comment = discussion.comments.build(new_comment_params)
-    if comment.save
-
-    else
-
-    end
-  end
-
-  def update
-  end
-
-  def rate
-  end
-
-  def destroy
+    comment.save
+    redirect_to discussion
   end
 
   private
   def new_comment_params
-    params.require(:comment).permit(:content, :parent_id, :discussion_id)
+    params.require(:comment).permit(:content, :parent_id)
   end
 
   def update_comment_params
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,41 @@
 class Comment < ActiveRecord::Base
 
   validates :discussion, presence: true
+  validate :parent_comment_belongs_to_same_discussion, unless: -> { persisted? }
 
-  belongs_to :discussion
-  has_one(
-    :parent,
+  before_save :append_quote_from_parent_comment, unless: -> { persisted? }
+
+  belongs_to :discussion, inverse_of: :comments
+
+  belongs_to(
+    :parent_comment,
     class_name: "Comment",
     foreign_key: :parent_id,
     primary_key: :id
   )
+
+  has_many(
+    :child_comments,
+    class_name: "Comment",
+    foreign_key: :parent_id,
+    primary_key: :id
+  )
+
+  private
+  # These validations/callbacks only get performed when a comment is
+  # being added for the first time (there's an unless persisted? above)
+  def parent_comment_belongs_to_same_discussion
+    return if parent_id.nil?
+    parent = Comment.find(parent_id)
+    if parent.discussion_id != self.discussion_id
+      errors[:parent_comment] << "is not part of the same discussion"
+    end
+  end
+
+  def append_quote_from_parent_comment
+    return if parent_id.nil?
+    parent = Comment.find(parent_id)
+    self.quote = parent.content
+  end
 
 end
